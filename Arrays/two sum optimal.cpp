8001dd77277f254a7f34364ccbdb8e7d5572de0e
Tahsin007 <77976636+Tahsin007@users.noreply.github.com>
2023-07-20 16:25:41 +0600

    Create two sum optimal.cpp

---
+++ Arrays/two sum optimal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        int n=nums.size();
+        vector<int> v;
+        for(int i=0;i<n;i++){
+            int a =nums[i];
+            int more = target-a;
+            if(mp.find(more)!=mp.end()){
+                
+                return {mp[more],i};
+            }
+            mp[a]=i;
+        }
+        return {};
+
+    }
+};
