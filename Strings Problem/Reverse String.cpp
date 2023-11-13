10ae22fa905d94a0821ca441440a2775b9c3d858
Tahsin007 <77976636+Tahsin007@users.noreply.github.com>
2023-11-13 12:21:02 +0600

    Create Reverse String.cpp

---
+++ Strings Problem/Reverse String.cpp
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/reverse-words-in-a-string/submissions/1097692519/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string reverseWord(string s){
+    string tmp="";
+    string ans="";
+
+    int left=0;
+    int right=s.length()-1;
+
+    while(s[left]==' '){
+        left++;
+    }
+    while(s[right]==' '){
+        right--;
+    }
+
+    for(int i=left;i<=right;i++){
+        if(s[i]!=' '){
+            tmp+=s[i];
+        }else{
+            if(s[i-1]==' '){
+                left++;
+                continue;
+            }else{
+                ans=tmp+" "+ans;
+                tmp="";
+            }
+        }
+    }
+    ans = tmp + " " + ans;
+    ans.erase(ans.begin() + ans.length() - 1);
+    return ans;
+}
+
+int main(){
+    string s = reverseWord("  hello world  ");
+    cout<<s;
+}
